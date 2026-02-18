4a4e876a7c8dbc1bd2713516d6f9359b26386ed5
Saketh9186 <snadimpa@student.gitam.edu>
2026-02-18 02:54:39 +0000

    lab

--- Module 1/bubble_sort.c
+++ Module 1/bubble_sort.c
@@ -13,9 +13,9 @@ int main()
         scanf("%d", &arr[i]);
     }
 
-    for (i = 0; i < n - 1; i++)
+    for (i = 0; i < n; i++)
     {
-        for (j = 0; j < n - i - 1; j++)
+        for (j = 0; j < n - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
